6ffa1da4f4e26f508d6e7ff3cac6999357066aa6
louisane fozin <louisane.cynthia@gmail.com>
2018-04-18 18:27:10 -0400

    Update poetry.js

--- poetry.js
+++ poetry.js
@@ -1,6 +1,6 @@
 var ambsound = new Audio ('LiveRoomOut.mp3');
 
- for (var i = 0; i < 0.3; i = i+0.005){
+ for (var i = 0; i < 0.3; i = i+0.001){
  ambsound.loop = true;
  ambsound.play();
  ambsound.volume = i;
